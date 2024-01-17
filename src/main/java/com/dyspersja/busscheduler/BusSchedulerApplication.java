3ef043af9d59b939c483f6a4e8739ade06efb628
Dyspersja <dyspersjamusic@gmail.com>
2024-01-17 14:05:51 +0100

    Created and added spring project

---
+++ src/main/java/com/dyspersja/busscheduler/BusSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.dyspersja.busscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyspersja/busscheduler/BusSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyspersja.busscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
