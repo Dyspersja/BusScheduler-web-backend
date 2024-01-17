771cc0859850db55109d7bf0d47d7a34ed5339ec
Dyspersja <dyspersjamusic@gmail.com>
2024-01-17 14:32:09 +0100

    Added BusLine entity class

---
+++ src/main/java/com/dyspersja/busscheduler/database/busline/BusLine.java
@@ -0,0 +1,24 @@
+package com.dyspersja.busscheduler.database.busline;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "bus_lines", uniqueConstraints = {@UniqueConstraint(columnNames = {"line_number", "is_return_line"})})
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusLine {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "line_number", nullable = false)
+    private int lineNumber;
+
+    @Column(name = "is_return_line", nullable = false)
+    private boolean isReturnLine;
+}
