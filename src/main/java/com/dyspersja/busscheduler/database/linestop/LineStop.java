7bcaa68a81fb8c3ce8de36c5ea2818dd8883c20b
Dyspersja <dyspersjamusic@gmail.com>
2024-01-17 14:34:46 +0100

    Added LineStop entity class

---
+++ src/main/java/com/dyspersja/busscheduler/database/linestop/LineStop.java
@@ -0,0 +1,40 @@
+package com.dyspersja.busscheduler.database.linestop;
+
+import com.dyspersja.busscheduler.database.busline.BusLine;
+import com.dyspersja.busscheduler.database.busstop.BusStop;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "line_stops")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LineStop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "bus_line_id", nullable = false)
+    private BusLine busLine;
+
+    @ManyToOne
+    @JoinColumn(name = "bus_stop_id", nullable = false)
+    private BusStop busStop;
+
+    @Column(name = "is_start", nullable = false)
+    private boolean isStart;
+
+    @Column(name = "is_end", nullable = false)
+    private boolean isEnd;
+
+    @Column(name = "bus_stop_order", nullable = false)
+    private int busStopOrder;
+
+    @Column(name = "travel_time", nullable = false)
+    private int travelTime;
+}
